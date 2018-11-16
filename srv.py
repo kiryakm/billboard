7d00a70d7984127c881a6cd6e4560e795bc7f294
kiryakm <38107795+kiryakm@users.noreply.github.com>
2018-11-16 19:18:08 +0700

    16.11 end

--- srv.py
+++ srv.py
@@ -9,9 +9,9 @@ def upload():
     #получаю картинку в base64, название и номер картинки из принятого json
     img = request.json['data']
     name = request.json['name']
-    num = request.json['num']
+    #num = request.json['num']
     img = re.sub('data:image/png;base64,', '', img) #убираю лишнее из строки картинки
-    with open("img/" + name + "_" + num + ".png", "wb") as fh:  #декодирую из base64 и сохраняю в папку img
+    with open("img/" + name + ".png", "wb") as fh:  #декодирую из base64 и сохраняю в папку img
         fh.write(base64.b64decode(img))
     return render_template("index.html")
 
